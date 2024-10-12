fe233e3a101643efda6a5a5b60ee29faef01d1c7
andycai <huayicai@gmail.com>
2024-10-12 11:53:24 +0800

    Add next button in detail window.

--- public/js/app.js
+++ public/js/app.js
@@ -611,5 +611,37 @@ function logSystem() {
         },
 
         // end 统计接口
+
+        showNextLog() {
+            const currentIndex = this.logs.findIndex(log => log.id === this.selectedLog.id);
+            if (currentIndex < this.logs.length - 1) {
+                this.selectedLog = this.logs[currentIndex + 1];
+            } else {
+                // If it's the last log in the current page, load the next page
+                if (this.page * this.limit < this.total) {
+                    this.changePage(this.page + 1).then(() => {
+                        this.selectedLog = this.logs[0];
+                    });
+                } else {
+                    this.showNotification('已经是最后一条日志', 'warning');
+                }
+            }
+        },
+
+        showNextStat() {
+            const currentIndex = this.stats.findIndex(stat => stat.id === this.selectedStat.id);
+            if (currentIndex < this.stats.length - 1) {
+                this.showStatDetails(this.stats[currentIndex + 1]);
+            } else {
+                // If it's the last stat in the current page, load the next page
+                if (this.statsPage * this.statsLimit < this.statsTotal) {
+                    this.changeStatsPage(this.statsPage + 1).then(() => {
+                        this.showStatDetails(this.stats[0]);
+                    });
+                } else {
+                    this.showNotification('已经是最后一条统计数据', 'warning');
+                }
+            }
+        },
     }
 }
