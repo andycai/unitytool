f72cc24d6bb531a7cb9ddc69915dcfd1125489b2
Andy Cai <huayicai@gmail.com>
2024-11-13 10:04:48 +0800

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# 优化编译大小
+# go build -ldflags="-s -w" main.go
+
+# 打包 release 版本的 go 程序，windows 平台
+CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -ldflags="-s -w" -o stats_serve_windows.exe main.go
+
+# 打包 release 版本的 go 程序，linux 平台
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags="-s -w" -o stats_serve_linux main.go
+
+# 打包 release 版本的 go 程序，mac 平台
+CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -ldflags="-s -w" -o stats_serve_mac main.go
