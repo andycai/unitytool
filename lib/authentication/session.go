f023d2eb76c5a9b46e8b3a5552988377056c0177
Andy Cai <huayicai@gmail.com>
2024-12-16 17:18:39 +0800

    Add simple session

--- lib/authentication/session.go
+++ lib/authentication/session.go
@@ -2,15 +2,12 @@ package authentication
 
 import (
 	"database/sql"
-	"time"
+	"sync"
 
 	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/middleware/session"
-	"github.com/gofiber/storage/mysql/v2"
-	"github.com/gofiber/storage/sqlite3"
 )
 
-var storedAuthenticationSession *session.Store
+var sessionStore = sync.Map{}
 
 func SessionSetup(dbDriver string, db *sql.DB, dsn, tableName string) {
 	if dbDriver == "mysql" {
@@ -21,66 +18,25 @@ func SessionSetup(dbDriver string, db *sql.DB, dsn, tableName string) {
 }
 
 func sessionMySQLStart(db *sql.DB, tableName string) {
-	store := mysql.New(mysql.Config{
-		Db:    db,
-		Table: tableName,
-	})
-
-	authSession := session.New(session.Config{
-		Storage:    store,
-		Expiration: 24 * time.Hour,
-	})
-
-	storedAuthenticationSession = authSession
 }
 
 func sessionStart(dsn, tableName string) {
-	store := sqlite3.New(sqlite3.Config{
-		Database: dsn,
-		Table:    tableName,
-	})
-
-	authSession := session.New(session.Config{
-		Storage: store,
-	})
-
-	storedAuthenticationSession = authSession
 }
 
 func AuthStore(c *fiber.Ctx, userID uint) {
-	session, err := storedAuthenticationSession.Get(c)
-	if err != nil {
-		panic(err)
-	}
-
-	session.Set("authentication", userID)
-	if err := session.Save(); err != nil {
-		panic(err)
-	}
+	sessionStore.Store(c.IP(), userID)
 }
 
 func AuthGet(c *fiber.Ctx) (bool, uint) {
-	session, err := storedAuthenticationSession.Get(c)
-	if err != nil {
-		panic(err)
-	}
+	userID, ok := sessionStore.Load(c.IP())
 
-	value := session.Get("authentication")
-	if value == nil {
+	if !ok {
 		return false, 0
 	}
 
-	return true, value.(uint)
+	return true, userID.(uint)
 }
 
 func AuthDestroy(c *fiber.Ctx) {
-	session, err := storedAuthenticationSession.Get(c)
-	if err != nil {
-		panic(err)
-	}
-
-	session.Delete("authentication")
-	if err := session.Save(); err != nil {
-		panic(err)
-	}
+	sessionStore.Delete(c.IP())
 }
